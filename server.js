d5c8f67a6bc1d9ff1cc672daf6519c87d7d9f8cb
Christian Andersen <Chrisaleks@gmail.com>
2017-10-15 13:14:24 +0200

    Tiny visual improvements to test with

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use("/EventPhotos", express.static("EventPhotos"));
 
 const db = require('./routes/db.js');
 
