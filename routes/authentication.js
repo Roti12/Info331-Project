9cb2b2a981873d065adf0862882f499508f0c5c6
Roti12 <31616440+Roti12@users.noreply.github.com>
2017-11-26 17:12:07 +0100

    removal of unecessary code

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,44 +4,6 @@ const db = require('./db.js');
 
 module.exports = {
     
-    checkForPassword : function(eventCode, callback) {
-        var boolean;
-        
-        db.retrieveEvent(eventCode, function(data) {
-                checkData(data); 
-                callback(boolean);
-            
-        });
-        
-        
-        function checkData(data) {
-            
-            if(data.pass === "" || typeof data.pass === "undefined") {
-                boolean = false;
-            } else {
-                boolean = true;
-            }
-        }
-
-    },
-    
-    
-    checkIfCorrectlyEntered : function(eventCode, input, callback) {
-        var boolean; 
-        
-        db.retrieveEvent(eventCode, function(data) {
-            authenticate(input, data.pass, data.admPass);
-            callback(boolean);
-        });
-        
-        function authenticate(stringInput, optPassword, admPassword) {
-            if(stringInput === optPassword || stringInput === admPassword) boolean = true;
-            else boolean = false;
-        }
-        
-            
-    },
-
     authenticate: function(stringInput, optPassword, admPassword) {
         console.log("Input: " + stringInput + ", optPW: " + optPassword + ", adminPW: " + admPassword);
         if(stringInput === optPassword || stringInput === admPassword) return true;
